47b7e5d4aacf599e72f16cedc0f03b2ac4f5fbce
Dchaney-code <lolicouse616@gmail.com>
2025-11-15 22:45:29 +0000

    Added maintenance log functionality

--- Logbook.py
+++ Logbook.py
@@ -11,7 +11,19 @@ def add_new_device():
     'ward': device_ward,
     'maintenance_logs': []
     }
-    print (f"\nDevice {device_id} ({device_type}) added to the logbook successfully")
+    print (f"\nDevice {device_id} ({device_type}) added to the logbook successfully.")
+
+def maintenance_log_task():
+    print("\n--- Accessing maintenance log---")
+    device_id = input("Enter the ID of the device you want to log: ")
+    if device_id in Database:
+        print(f"---logging {device_id} ({Database[device_id]['type']}) ---")
+        maintenance_description = input("Please enter the description of the maintenance carried out on the device (e.g., calibrated sensor): ")
+        Database[device_id]['maintenance_log'].append(maintenance_description)
+        print(f"\n{device_id}s Maintenance has been successfully logged.")
+    else:
+        print(f"\nInvalid Device ID detected '{device_id}' is not found in the logbook please check for typo's and try again.")
+        print("Please Make sure the device has been registered to the database using option 1 from the main menu.")
 
 def main_menu():
     print("\n---Welcome to the Medical Devices Logbook---")
@@ -27,7 +39,7 @@ while True:
     if user_choice == '1':
         add_new_device()
     elif user_choice == '2':
-        print ("Log maintenance task WIP")
+        maintenance_log_task()
     elif user_choice == '3':
         print ("View device history WIP")
     elif user_choice == '4':
